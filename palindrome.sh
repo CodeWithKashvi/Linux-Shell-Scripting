76b7e9dae3bea4006d25d955047ffa6eefeb48c5
Kashvi Jain <kashvijain2910@gmail.com>
2026-02-04 01:14:12 +0530

    Added palindrome num shell scripts

---
+++ palindrome.sh
@@ -0,0 +1,27 @@
+echo "Enter a number:"
+read num
+orig=$num
+rev=0
+sum=0
+count=0
+
+while [ $num -gt 0 ]
+do
+    digit=`expr $num % 10`
+    rev=`expr $rev \* 10 + $digit`
+    sum=`expr $sum + $digit`
+    count=`expr $count + 1`
+    num=`expr $num / 10`
+done
+
+echo "Reversed number: $rev"
+echo "Sum of digits: $sum"
+echo "Count of digits: $count"
+
+if [ $orig -eq $rev ]
+then
+    echo "$orig is a palindrome"
+else
+    echo "$orig is not a palindrome"
+fi
+
