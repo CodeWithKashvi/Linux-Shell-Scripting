65ba59b898ff100475744632187fc74f79387be1
CodeWithKashvi <kashvijain2910@gmail.com>
2026-02-12 00:04:57 +0530

    Add comments to explain script functionality
    Added comments explaining the purpose of the script.

--- oddwhile.sh
+++ oddwhile.sh
@@ -1,4 +1,5 @@
-i=1
+#Aim: To write and execute a Shell script that uses a while loop and conditional statements to iterate numbers 
+#from 1 to 19 and display only the odd numbers on the screen.i=1
 while [ $i -lt 20 ]
 do 
 	if test `expr $i \% 2` -ne 0
