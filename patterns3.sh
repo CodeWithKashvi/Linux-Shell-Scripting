c0e6942ac34ca675b49cc40cc3b36ec26e037881
CodeWithKashvi <kashvijain2910@gmail.com>
2026-02-12 00:19:58 +0530

    Add comments to patterns3.sh for clarity
    Added comments to explain the purpose of the script.

--- patterns3.sh
+++ patterns3.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#Aim:To write and execute a Shell script that uses nested loops to print a descending numeric pattern
+#        3 2 1
+#        3 2
+#        3
+
 
 for ((i=1; i<=3; i++))
 do
